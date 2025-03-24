3fecd32cd68cdb397aa0317497041f44ffc29ba9
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-24 21:20:26 +1300

    use frames to organise labels

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 
-def greet() -> None:
-    """print a greeting"""
-    print(f"Hello {user_name.get().title() or "World"}!")  # prints Hello and the user's name or hello world when no name is provided
-
-
 root = tk.Tk()
 
-user_name = tk.StringVar()
+main = ttk.Frame(root)
+main.pack(side="left", fill="both", expand=True)
 
-name_label = ttk.Label(root, text="Name: ")
-name_label.pack(side="left")
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
-name_entry.pack(side="left")
-name_entry.focus()
+tk.Label(main, text="Label top (1)", bg="red").pack(side="top", expand=True, fill="both")
+tk.Label(main, text="Label top (2)", bg="orange").pack(side="top", expand=True, fill="both")
+tk.Label(root, text="Label left", bg="green").pack(side="left", expand=True, fill="both")
 
-
-greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack(side="left")
-
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack(side="right")
 
 root.mainloop()
