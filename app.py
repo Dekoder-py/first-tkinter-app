7480e2225672659a70906f3758e6fc0f99e704ba
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-24 19:12:21 +1300

    create greet button and quit button

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
 
+def greet() -> None:
+    """print a greeting"""
+    print("Hello world!")
+
+
 root = tk.Tk()
-ttk.Label(root, text="Hello, World!", padding=(40, 20)).pack()
+
+greet_button = ttk.Button(root, text="Greet", command=greet)
+greet_button.pack()
+
+quit_button = ttk.Button(root, text="Quit", command=root.destroy)
+quit_button.pack()
 
 root.mainloop()
