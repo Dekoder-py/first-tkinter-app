edaac8cc2e437e2d9d953e68aa0793a1225d1a28
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-24 19:18:53 +1300

    make buttons fill the screen

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ def greet() -> None:
 
 root = tk.Tk()
 
+# change theme to make it work like it should (i.e. how it works on windows)
+s = ttk.Style()
+s.theme_use("clam")
+
 greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack()
+greet_button.pack(side="top", fill="both", expand=True)
 
 quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack()
+quit_button.pack(side="top", fill="both", expand=True)
 
 root.mainloop()
