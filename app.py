11b4977bead916cf6448a04c7716723d02b55a05
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-24 21:28:08 +1300

    use frames to organise greeting app

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import tkinter as tk
 from tkinter import ttk
 
+def greet() -> None:
+    """print a greeting"""
+    print(f"Hello {user_name.get().title() or "World"}!")  # prints Hello and the user's name or hello world when no name is provided
+
+
 root = tk.Tk()
 
-main = ttk.Frame(root)
-main.pack(side="left", fill="both", expand=True)
+user_name = tk.StringVar()
 
-tk.Label(main, text="Label top (1)", bg="red").pack(side="top", expand=True, fill="both")
-tk.Label(main, text="Label top (2)", bg="orange").pack(side="top", expand=True, fill="both")
-tk.Label(root, text="Label left", bg="green").pack(side="left", expand=True, fill="both")
+input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
+input_frame.pack(fill="both")
 
+name_label = ttk.Label(input_frame, text="Name: ")
+name_label.pack(side="left")
+name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
+
+
+buttons = ttk.Frame(root, padding=(20, 10))
+buttons.pack(fill="both")
+
+greet_button = ttk.Button(buttons, text="Greet", command=greet)
+greet_button.pack(side="left")
+
+quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
+quit_button.pack(side="left")
 
 root.mainloop()
